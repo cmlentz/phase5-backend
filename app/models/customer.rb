ffb8f9fc0635452cc51879659bc7f09cfef1b032
Chris <cmlentz76@gmail.com>
2021-10-09 02:18:17 -0400

    fixed offline at least

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
-  has_many :registers, dependent: :destroy
+  has_many :registers
   has_many :activities, through: :registers
+
+  validates :name, presence: true
+  validates :age, presence: true
 end
